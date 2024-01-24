29ba63f42685345164d74cd2f6fd622b74a08d5c
Meester-Moo <aidan.roraback7@gmail.com>
2024-01-23 19:44:21 -0800

    changed some stuff around and added LeetCode folder

---
+++ A Common Sense Guide to Data Structures and Algorithms/Trees/trie.py
@@ -0,0 +1,11 @@
+class TrieNode:
+  def __init__(self):
+    self.children = {}
+
+class Trie:
+  def __init__(self):
+    self.root = TrieNode()
+
+node1 = TrieNode()
+node2 = TrieNode()
+node3 = TrieNode()
